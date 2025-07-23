0d3bc4a56ca664d405f8bb91cc8d45e409423dc6
Axel Nadir <axeltettw@gmail.com>
2025-07-23 00:37:46 -0600

    Nuevos cambios con cors hacia el front hosteado

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://microservicio-auth-view.vercel.app'
+];
+
 app.use(cors({
-  origin: "http://localhost:5173", // frontend exacto
-  credentials: true                // permite cookies o headers con auth
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
 
